65b876bea8c4c4735a1a90b39fe9500eea3d6871
Sergey Stolz <Sergeystolz13@icloud.com>
2021-09-20 15:26:26 +0300

    Initial Commit

---
+++ Simple Animations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Animations
+//
+//  Created by mac on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
